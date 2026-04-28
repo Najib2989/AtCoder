2d4e6b2c70838e0aab7bec8d0acceae2d7140fa5
Najib2989 <mahmudnajib26@gmail.com>
2026-04-29 02:02:22 +0600

    Add files via upload

---
+++ Cut.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int arr[N];
+    int K;
+    cin >> K;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+      //K = K % N;
+
+    int ind = N-K;
+    for(int i=ind; i<N; i++){
+        cout << arr[i] << " ";
+    }
+
+     for(int i=0; i<ind; i++){
+        cout << arr[i] << " ";
+     }
+
+
+  return 0;
+}
