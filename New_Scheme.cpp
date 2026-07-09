b14c6ef26e41ff512374f1ee031178637446edb8
Najib2989 <mahmudnajib26@gmail.com>
2026-07-10 03:00:28 +0600

    Add files via upload

---
+++ New_Scheme.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int arr[8];
+   for(int i=0; i<8; i++){
+    cin >> arr[i];
+   }
+
+   int flag = 1;
+
+   for(int i=0; i<8; i++){
+    if(arr[i]<100 || arr[i]>675){
+        flag = 0;
+        break;
+    }
+   }
+
+   for(int i=0; i<8; i++){
+    if(arr[i] % 25!=0){
+        flag = 0;
+        break;
+    }
+   }
+
+   for(int i=0; i<7; i++){
+    if(arr[i] > arr[i+1]){
+        flag = 0;
+        break;
+    }
+   }
+
+   if(flag == 1){
+    cout << "Yes" << endl;
+   }else{
+    cout << "No" << endl;
+   }
+return 0;
+}
